bf1163312072c83189f6323d485a0bf9291f22a9
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-03-03 20:08:45 +0200

    FIX

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const cartBtn = document.getElementById("cart-btn");
 const clearCartBtn = document.getElementById("clear-cart-btn");
 const totalNumberOfItems = document.getElementById("total-items");
 const cartSubTotal = document.getElementById("subtotal");
-const cartTaxes = document.getElementById("taxes");
 const cartTotal = document.getElementById("total");
 const showHideCartSpan = document.getElementById("show-hide-cart");
 let isCartShowing = false;
